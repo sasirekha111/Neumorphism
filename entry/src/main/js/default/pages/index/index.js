55a774e1a38126d086c307352e4046308e6f24ce
Aryan Gupta <arynn.gupta@gmail.com>
2021-12-12 03:31:34 +0530

    added theme and fixed bugs

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,28 @@
 export default {
-    data: {
-        title: ""
+    theme: {
+        r: 241,
+        g: 206,
+        b: 242,
     },
-    onInit() {
-        this.title = this.$t('strings.world');
-    }
+    buttonClick(){
+        console.log('Button clicked !');
+    },
+    checkboxClick(){
+        console.log('Checkbox clicked !')
+    },
+    radioClick(){
+        console.log('Radio clicked !')
+    },
+    toggleClick(){
+        console.log('Toggle clicked !')
+    },
+    dropdownSelect(e){
+        console.log(e.newValue)
+    },
+    previousEvent(){
+        console.log("goto PreviousPage")
+    },
+    nextEvent(){
+        console.log("goto NextPage")
+    },
 }
